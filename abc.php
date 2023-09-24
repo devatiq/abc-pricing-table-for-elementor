a6d9592b0e984c0260c6ec461c6c88a3305e38e4
devatiq <devatiqbd@gmail.com>
2023-09-24 22:18:34 +0600

    name

---
+++ abc.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Plugin Name: ABC Pricing Table for Elementor
+Plugin URI: http://www.abctheme.com/
+Description: This is a required plugin for Abcbiz Theme Elementor Version.
+Version: 1.1.0
+Author: ABCTHEME
+Author URI: http://www.abctheme.com/
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: ABCPTE
+Domain Path: /languages
+Elementor tested up to: 3.16.0
+Elementor Pro tested up to: 3.15.1
+*/
