da746314d7e6dfac9b17ec02119575639ffa07d0
hmbashar <hmbashar@gmail.com>
2023-09-25 11:48:36 +0600

    change plugin basic information

--- abc.php
+++ abc.php
@@ -1,15 +1,15 @@
 <?php 
 /*
 Plugin Name: ABC Pricing Table for Elementor
-Plugin URI: http://www.abctheme.com/
+Plugin URI: https://github.com/devatiq/abc-pricing-table-for-elementor
 Description: This is a required plugin for Abcbiz Theme Elementor Version.
-Version: 1.1.0
-Author: ABCTHEME
-Author URI: http://www.abctheme.com/
+Version: 1.0
+Author: SupreoX Limited
+Author URI: https://supreox.com
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: ABCPTE
 Domain Path: /languages
 Elementor tested up to: 3.16.0
-Elementor Pro tested up to: 3.15.1
+Elementor Pro tested up to: 3.16.1
 */
